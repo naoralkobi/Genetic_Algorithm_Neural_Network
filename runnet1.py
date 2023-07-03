b152fae16b27400e441a19f167ec4800012bfa57
AvivHarel <90348080+AvivHarel@users.noreply.github.com>
2023-07-03 14:42:45 +0300

    Create runnet1.py

---
+++ runnet1.py
@@ -0,0 +1,40 @@
+"""
+Script to load a test dataset, use a pre-trained neural network for predictions, and save the results to a file.
+"""
+
+import pandas as pd
+from buildnet0 import NeuralNetwork  # Assuming `buildnet0` contains the definition of the NeuralNetwork class
+import numpy as np
+import pickle
+
+
+def load(filename):
+    """
+    Load data from a file.
+
+    Args:
+        filename (str): The name of the file to load.
+
+    Returns:
+        numpy.ndarray: The loaded data as a NumPy array.
+
+    """
+    data = np.loadtxt(filename, dtype=int)
+    return data
+
+
+if __name__ == "__main__":
+    # Load test dataset
+    x_test = load("testnet1.txt")
+
+    # Load pre-trained neural network
+    with open('wnet1.pkl', 'rb') as f:
+        best_network_result = pickle.load(f)
+
+    # Make predictions using the loaded network
+    predictions = best_network_result.predict(x_test)
+
+    # Save predictions to a file
+    with open("result1.txt", "w") as file:
+        for label in predictions:
+            file.write(str(label) + "\n")
